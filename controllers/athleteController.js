396f475c8a7fe82d1ab80451666e87803076a091
Richard Walsh <richwalsh2@googlemail.com>
2023-09-21 21:31:24 +0100

    add athlete controllers for get athlete and register athlete

--- controllers/athleteController.js
+++ controllers/athleteController.js
@@ -1,3 +1,34 @@
-exports.athlete = async (req, res) => {};
+const fetch = require('node-fetch');
 
-exports.athleteRegister = async (req, res) => {};
+exports.athlete = async (req, res) => {
+  try {
+    if (!req.query.uid) {
+      throw new Error('athlete not found');
+    }
+
+    const uid = req.query.uid;
+    console.log(uid);
+    const athleteReq = await fetch(
+      `http://178.62.94.235/mooch/athlete/?userId=${uid}`
+    );
+    const athleteData = await athleteReq.json();
+    res.json({ data: athleteData });
+  } catch (error) {
+    console.log(error);
+    res.json({ error, message: error.message });
+  }
+};
+
+exports.athleteRegister = async (req, res) => {
+  try {
+    await fetch(`http://178.62.94.235/mooch/athlete/?userId=${uid}`, {
+      method: 'POST',
+      body: req.body,
+    });
+
+    res.status(200).send('registration successfull');
+  } catch (error) {
+    console.log(error);
+    res.json({ error, message: error.message });
+  }
+};
--- routes/athleteRoutes.js
+++ routes/athleteRoutes.js
@@ -1,23 +1,17 @@
 const router = require('express').Router();
-const fetch = require('node-fetch');
+const { request, response } = require('express');
+// const fetch = require('node-fetch');
+const athleteController = require('../controllers/athleteController');
 
-router.get('/', async (req, res) => {
-  try {
-    if (!req.query.uid) {
-      throw new Error('athlete not found');
-    }
-
-    const uid = req.query.uid;
-    console.log(uid);
-    const athleteReq = await fetch(
-      `http://178.62.94.235/mooch/athlete/?userId=${uid}`
-    );
-    const athleteData = await athleteReq.json();
-    res.json({ data: athleteData });
-  } catch (error) {
-    console.log(error);
-    res.json({ error, message: error.message });
-  }
-});
+router
+  .route('/')
+  .get(athleteController.athlete)
+  .post(athleteController.athleteRegister);
+// router.get('/', (request, response) => {
+//   console.log('hello');
+//   response.json({
+//     message: 'hello',
+//   });
+// });
 
 module.exports = router;
